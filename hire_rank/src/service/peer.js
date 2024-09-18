53a86bab730ac12e70007175deafe1fe83d4c557
Parthik Diyora <ddiyora503@gmail.com>
2024-09-18 22:14:01 +0530

    creating rtc peerconnection throw stun server

--- hire_rank/src/service/peer.js
+++ hire_rank/src/service/peer.js
@@ -39,3 +39,24 @@ class PeerService {
 }
 
 export default new PeerService();
+
+
+
+
+
+
+// class PeerService {
+//   constructor() {
+//     if (!this.peer) {
+//       this.peer = new RTCPeerConnection({
+//         iceServers: [
+//           {
+//             urls: [
+//               "stun:stun.l.google.com:19302",
+//               "stun:global.stun.twilio.com:3478",
+//             ],
+//           },
+//         ],
+//       });
+//     }
+  
